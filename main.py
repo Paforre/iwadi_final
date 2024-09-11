e8b1e4b5cdb2b213828dc98248d2e015dfbe0deb
Régis Padonou <regis.padonou.5@gmail.com>
2024-09-11 12:49:44 +0100

    Deuxieme commit

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from  search import search_route
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+origins = [
+    "*",
+]
 # Base Endpoint
 
 
